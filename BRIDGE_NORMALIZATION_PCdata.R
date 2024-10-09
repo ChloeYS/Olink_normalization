9906b0b4567796b0d534b9be4c0c8f4f55b4de71
chloe <31994064+ChloeYS@users.noreply.github.com>
2024-10-09 12:13:43 -0400

    Fix the format of the outputted files with bridged data + change the argument list for bash script in order to separate input files for normalization vs validation scripts

--- BRIDGE_NORMALIZATION_PCdata.R
+++ BRIDGE_NORMALIZATION_PCdata.R
@@ -1,7 +1,7 @@
 # FILENAME: BRIDGE_NORMALIZATION_PCdata.R
 #Last updated October 2024
 
-#Usage: gets sourced by other files.
+#Usage: to create the bridged data files, sources other files and output csv is sourced by other files.
 
 cat("\n\n\n\n###############################################################################################\n",
             "1. SOURCE PACKAGES AND FUNCTIONS\n",
@@ -18,17 +18,19 @@ library(OlinkAnalyze)
 cat("\n\n\n\n###############################################################################################\n",
             "2. LOAD DATAFRAME\n",
             "###############################################################################################\n\n\n")
+    
+    # #If getting end of line error or other bug related to csv reading:
+    # ##To run only without the arg.vec part in bash Run_ script, or directly on the Rscript
+    # filepaths <- c("/Users/nikhilbhagwat/Desktop/0_DATA/0.4_Biofluids/0.4.3_Olink/0.4.4_Olink_PC_data/2238A_Tartaglia_NPX_IPCnormalized2024-08-08.csv", #Plate A NPX data            
+    #                 "/Users/nikhilbhagwat/Desktop/0_DATA/0.4_Biofluids/0.4.3_Olink/0.4.4_Olink_PC_data/2238B_Tartaglia_EXTENDED_NPX_2024-09-30_PCnormalized/Tartaglia_EXTENDED_NPX_2024-09-30.csv", #Plate B NPX data
+    #                 "/Users/nikhilbhagwat/Desktop/0_DATA/0.4_Biofluids/0.4.3_Olink/0.4.4_Olink_PC_data/2238C_Tartaglia_EXTENDED_NPX_2024-03-25_PC_normalized.csv", #Plate C NPX
+    #                 "/Users/nikhilbhagwat/Desktop/0_DATA/0.4_Biofluids/0.4.3_Olink/0.4.3.1_Simrika_Onedrive/Second_project_AD_HC/Sample_all_2ndproject.csv", #Merged IDs from Simrika: not needed here but needed for TEMPLATE_CREATION.R
+    #                 "/Users/nikhilbhagwat/Desktop/0_DATA/0.4_Biofluids/0.4.3_Olink/0.4.3.1_Simrika_Onedrive/Third_project_Genetic_cases/Sample_template_3rd_olink.csv", #Merged IDs from Simrika: not needed here but needed for TEMPLATE_CREATION.R
+    #                 "all_template_df.csv") #Template file with all TARTAGLIA ID to Aliquot ID (=SAMPLEID) match keys
 
-#If getting end of line error or other bug related to csv reading:
-filepaths <- c("/Users/nikhilbhagwat/Desktop/0_DATA/0.4_Biofluids/0.4.3_Olink/0.4.4_Olink_PC_data/2238A_Tartaglia_NPX_IPCnormalized2024-08-08.csv", #Plate A NPX data            
-                "/Users/nikhilbhagwat/Desktop/0_DATA/0.4_Biofluids/0.4.3_Olink/0.4.4_Olink_PC_data/2238B_Tartaglia_EXTENDED_NPX_2024-09-30_PCnormalized/Tartaglia_EXTENDED_NPX_2024-09-30.csv", #Plate B NPX data
-                "/Users/nikhilbhagwat/Desktop/0_DATA/0.4_Biofluids/0.4.3_Olink/0.4.4_Olink_PC_data/2238C_Tartaglia_EXTENDED_NPX_2024-03-25_PC_normalized.csv", #Plate C NPX
-                "/Users/nikhilbhagwat/Desktop/0_DATA/0.4_Biofluids/0.4.3_Olink/0.4.3.1_Simrika_Onedrive/Second_project_AD_HC/Sample_all_2ndproject.csv", #Merged IDs from Simrika: not needed here but needed for TEMPLATE_CREATION.R
-                "/Users/nikhilbhagwat/Desktop/0_DATA/0.4_Biofluids/0.4.3_Olink/0.4.3.1_Simrika_Onedrive/Third_project_Genetic_cases/Sample_template_3rd_olink.csv", #Merged IDs from Simrika: not needed here but needed for TEMPLATE_CREATION.R
-                "all_template_df.csv") #Template file with all TARTAGLIA ID to Aliquot ID (=SAMPLEID) match keys
+    # filepaths_df <- data.frame(filepaths)
+    # write.csv(filepaths_df, "filepaths_normalization.csv") #a copy of object created at the time of submission was kept for reference. Can be shared upon request. 
 
-filepaths_df <- data.frame(filepaths)
-write.csv(filepaths_df, "filepaths.csv") #a copy of object created at the time of submission was kept for reference. Can be shared upon request. 
 
 #Read the csv that contains the pths to the plates
 filepaths_df <- read.func(arg.vec[1], 'filepaths_df') #arg.vec[1] is the csv with the pathways to the different data spreadsheets. 
@@ -290,7 +292,7 @@ BvsC_bridged_df <- olink_normalization_bridge(project_1_df = npx_1, project_2_df
 
 
 cat("\n\n\n\n###############################################################################################\n",
-            "12. QC OF PLATES A TO C BRIDGING \n",
+            "12. QC OF PLATES A TO C BRIDGING: OLINKANALYZE \n",
             "###############################################################################################\n\n\n")
 
 # Plot NPX density after bridging normalization
@@ -307,29 +309,41 @@ AvsC_bridged_data <- AvsC_bridged_df %>%
   mutate(Type = ifelse(Freezer_ID %in% AtoC_overlapping_FreezerID, paste(Project, "Bridge"), paste(Project, "Sample")))
 
 # PCA plot
-pca_plot_AvsC <- olink_pca_plot(df = AvsC_bridged_data, color_g = "Type", byPanel= TRUE)
+olink_pca_plot(df = AvsC_bridged_data, color_g = "Type", byPanel= TRUE)
 
-str(pca_plot_AvsC)
+cat("\n\n\n\n###############################################################################################\n",
+            "13. QC OF PLATES B TO C BRIDGING: OLINKANALYZE \n",
+            "###############################################################################################\n\n\n")
+
+# Plot NPX density after bridging normalization
+BvsC_bridged_df %>%
+  mutate(Panel = gsub("Olink ", "", Panel)) %>%
+  ggplot(aes(x = NPX, fill = PlateID)) + geom_density(alpha = 0.3) + 
+  facet_grid(~Panel) + set_plot_theme() +
+  ggtitle("Plate B vs Plate C pre-bridging: NPX distribution") +
+  scale_fill_manual('Plate ID', values=c('red', 'darkblue')) +
+  theme(strip.text = element_text(size = 16),legend.position = "top")
+
+# After bridging: PCA plot
+BvsC_bridged_data <- BvsC_bridged_df %>%
+  mutate(Type = ifelse(Freezer_ID %in% BtoC_overlapping_FreezerID, paste(Project, "Bridge"), paste(Project, "Sample")))
+
+# PCA plot
+olink_pca_plot(df = BvsC_bridged_data, color_g = "Type", byPanel= TRUE)
 
 
-# cat("\n\n\n\n###############################################################################################\n",
-#             "13. QC OF PLATES B TO C BRIDGING \n",
-#             "###############################################################################################\n\n\n")
+cat("\n\n\n\n###############################################################################################\n",
+            "14. SAVING THE BRIDGED DATA \n",
+            "###############################################################################################\n\n\n")
 
-# # Plot NPX density after bridging normalization
-# BvsC_bridged_df %>%
-#   mutate(Panel = gsub("Olink ", "", Panel)) %>%
-#   ggplot(aes(x = NPX, fill = PlateID)) + geom_density(alpha = 0.3) + 
-#   facet_grid(~Panel) + set_plot_theme() +
-#   ggtitle("Plate B vs Plate C pre-bridging: NPX distribution") +
-#   scale_fill_manual('Plate ID', values=c('red', 'darkblue')) +
-#   theme(strip.text = element_text(size = 16),legend.position = "top")
+AvsC_bridged_df %>% 
+  dplyr::filter(Project == "data2") %>% 
+  dplyr::select(-Project, -Adj_factor) %>% 
+  write.csv(, file = "AtoC_bridged_PC.csv")
 
-# # After bridging: PCA plot
-# BvsC_bridged_data <- BvsC_bridged_df %>%
-#   mutate(Type = ifelse(Freezer_ID %in% BtoC_overlapping_FreezerID, paste(Project, "Bridge"), paste(Project, "Sample")))
 
-# # PCA plot
-# pca_plot_BvsC <-<- olink_pca_plot(df = BvsC_bridged_data, color_g = "Type", byPanel= TRUE)
-
+BvsC_bridged_df %>% 
+  dplyr::filter(Project == "data2") %>% 
+  dplyr::select(-Project, -Adj_factor) %>% 
+  write.csv(, file = "BtoC_bridged_PC.csv")
 
---
+++ Run_BRIDGE_NORMALIZATION_PCdata.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#bash BRIDGE_NORMALIZATION.R
+
+Rscript BRIDGE_NORMALIZATION_PCdata.R /Users/nikhilbhagwat/projects/git_repos/Olink_normalization/filepaths_normalization.csv
+
+
